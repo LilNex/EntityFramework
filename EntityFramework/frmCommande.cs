5a2e9449777e874f7aba7c011f35c05ac2eb485a
LilNex <nextorchq@gmail.com>
2022-02-02 22:31:06 +0100

    Reset projet

--- EntityFramework/frmCommande.cs
+++ EntityFramework/frmCommande.cs
@@ -19,21 +19,11 @@ namespace EntityFramework
 
         private void frmCommande_Load(object sender, EventArgs e)
         {
-            //db = new modelEntities();
-            cmbclient.DataSource = clsDb.db.clients.ToList() ;
-            cmbclient.DisplayMember = "nom";
-            cmbclient.ValueMember = "id";
 
-            comboBox2.DataSource = clsDb.db.clients.ToList();
-            comboBox2.DisplayMember = "nom";
-            comboBox2.ValueMember = "id";
-
-            dataGridView1.DataSource = clsDb.db.commandes.ToList();
         }
 
         private void btnAjouter_Click(object sender, EventArgs e)
         {
-            clsDb.AjouterCommande(dateTimePicker1.Value, float.Parse(txtMontant.Text), int.Parse(cmbclient.SelectedValue.ToString()));
 
         }
 
@@ -49,41 +39,21 @@ namespace EntityFramework
 
         private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
         {
-            int i = e.RowIndex;
-            commande obj = (commande)dataGridView1.Rows[i].DataBoundItem;
-            txtId.Text = obj.id.ToString();
-            txtMontant.Text = obj.montant.ToString();
-            dateTimePicker1.Value = DateTime.Parse(obj.DateCmd.ToString());
-            cmbclient.SelectedItem = obj.client;
+
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            viderForm();
         }
 
         public void viderForm()
         {
-            txtId.Text = "";
-            txtMontant.Text = String.Empty;
-            dateTimePicker1.Value = DateTime.Now;
-            cmbclient.Text = "";
+  
         }
 
         private void btnRechercher_Click(object sender, EventArgs e)
         {
-            int id = int.Parse(txtId.Text);
-
-            var req2 = clsDb.db.commandes.Where(
-                (commande c) => c.id == id
-            );
-
-            var req = from x in clsDb.db.commandes
-                      where x.id == id
-                      join cl in clsDb.db.clients on x.client equals cl
-                      select new { x, cl };
-
-            dataGridView1.DataSource = req.ToList();
+   
         }
     }
 }
