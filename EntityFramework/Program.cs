ea7d3d4b3f46e612c2d17dc53dfa2f0a6d3cb221
LilNex <nextorchq@gmail.com>
2022-02-02 22:17:58 +0100

    Formulaire ajout commande

--- EntityFramework/Program.cs
+++ EntityFramework/Program.cs
@@ -16,7 +16,7 @@ namespace EntityFramework
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmStagiaire());
+            Application.Run(new frmCommande());
         }
     }
 }
---
+++ EntityFramework/clsDb.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework
+{
+    class clsDb
+    {
+         public static modelEntities db = new modelEntities();
+
+
+        DateTime derniereValidation { get; set; }
+
+        public static void AjouterCommande(DateTime dateCmd, float montant, int idClient)
+        {
+            commande c = new commande();
+            c.DateCmd = dateCmd;
+            c.montant = montant;
+            c.idClient = idClient;
+            Console.WriteLine("Commande crée");
+
+            clsDb.db.commandes.Add(c);
+            clsDb.db.SaveChanges();
+
+            Console.WriteLine("Commande ajoutée");
+        }
+
+
+
+    }
+
+}
